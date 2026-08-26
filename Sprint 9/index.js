25affa69a85cccb9560229d1dd216e4f37d6644f
Amit Sikdar <sikdara477@gmail.com>
2026-08-26 15:43:12 +0530

    phase 2 commit

--- Sprint 9/index.js
+++ Sprint 9/index.js
@@ -6,25 +6,68 @@ const PORT = 5000;
 
 app.use(express.json());
 
+let blogPosts = [
+    {
+        id: 1,
+        title: "First Post",
+        content: "Hello World!"
+    }
+];
+
+
 app.get('/posts',(req, res) => {
-    res.json({ message: 'Route Active'});
+    res.status(200).json(blogPosts);
 });
+
 
 app.get('/posts/:id',(req, res) => {
-    res.json({ message: 'Route Active'});
+    const id = parseInt(req.params.id);
+    const postFound = blogPosts.find(post => post.id === id);
+    if (postFound) {
+        return res.status(200).json(postFound);
+    } else {
+        return res.status(404).json({message: "Post not found"});
+    }
 });
+
 
 app.post('/posts',(req, res) => {
-    res.json({ message: 'Route Active'});
+    const { title, content } = req.body;
+    let newPost = {
+        id: Date.now(),
+        title: title,
+        content: content
+    }
+    blogPosts.push(newPost);
+    res.status(201).json({message: "Post created successfully"});
 });
+
 
 app.put('/posts/:id',(req, res) => {
-    res.json({ message: 'Route Active'});
+    const { title, content } = req.body;
+    const id = parseInt(req.params.id);
+    const postFound = blogPosts.find(post => post.id === id);
+    if (postFound) {
+        postFound.title = title;
+        postFound.content = content;
+        return res.status(200).json({message: "Post updated successfully"});
+    } else {
+        return res.status(404).json({message: "Post not found"});
+    }
 });
 
+
 app.delete('/posts/:id',(req, res) => {
-    res.json({ message: 'Route Active'});
+    const id = parseInt(req.params.id);
+    let postFound = blogPosts.find(post => post.id === id);
+    if (postFound) {
+        blogPosts = blogPosts.filter(post => post.id !== id);
+        return res.status(200).json({message: "Post deleted successfully"});
+    } else {
+        return res.status(404).json({message: "Post not found"});
+    }
 });
+
 
 
 app.listen(PORT, () => {
